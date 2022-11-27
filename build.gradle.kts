db657fb64dc14b6b20fb63c2b9abb6999bd3cd99
cjl_macbook <cjl_spy@163.com>
2022-11-27 21:51:45 +0800

    Update maven central sign config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     `maven-publish`
+    signing
+    id("com.vanniktech.maven.publish") version "0.22.0"
 }
 
 group = "top.kikt"
@@ -29,4 +31,46 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+// about publish
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.S01, true)
+    signAllPublications()
+
+    pom {
+        name.set("PdfToPrinter")
+        description.set("Pdf format to pwg or pclm")
+        inceptionYear.set("2022")
+        url.set("https://github.com/CaiJingLong/PdfToPrinter")
+        licenses {
+            license {
+                name.set("MIT Style")
+                url.set("https://github.com/CaiJingLong/PdfToPrinter/blob/main/LICENSE")
+                distribution.set("https://github.com/CaiJingLong/PdfToPrinter/blob/main/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("caijinglong")
+                name.set("Cai Jing Long")
+                url.set("https://github.com/caijinglong/")
+            }
+        }
+        scm {
+            url.set("https://github.com/CaiJingLong/PdfToPrinter")
+            connection.set("https://github.com/CaiJingLong/PdfToPrinter.git")
+            developerConnection.set("https://github.com/CaiJingLong/PdfToPrinter.git")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "LocalBuildDir"
+            url = uri("${buildDir}/publishing-repository")
+        }
+    }
 }
